5918410b3a9f2cf7621b0a7ce66ca99c0dcdd4a8
again7536 <45936642+again7536@users.noreply.github.com>
2021-10-29 19:42:12 +0900

    [Add] src

---
+++ src/Assets/Example/ToggleButton.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleButton : UdonSharpBehaviour
+{
+    public GameObject door;
+    bool is_open = false;
+    Animator doorAnimator;
+
+    public void Start()
+    {
+        doorAnimator = door.GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "onDoorOpen");
+    }
+
+    public void onDoorOpen()
+    {
+        is_open = !is_open;
+        doorAnimator.SetBool("isOpen", is_open);
+    }
+
+}
