392c044040ae3e3dd05a44729e8af5a51d616b37
yunminjin2 <56184924+yunminjin2@users.noreply.github.com>
2021-11-25 22:37:32 +0900

    udpate udon

---
+++ src/Assets/Scripts/moveLight.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class moveLight : UdonSharpBehaviour
+{
+    public GameObject light;
+    void Start()
+    {
+          
+    }
+
+    public override void Interact()
+    {
+        light.transform.Translate(0.2f, 0, 0);
+
+    }
+}
