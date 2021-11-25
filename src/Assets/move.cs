899be282b0528ac653cd3434c72d43ffd6c83cc3
yunminjin2 <56184924+yunminjin2@users.noreply.github.com>
2021-11-25 19:27:20 +0900

    update 2

---
+++ src/Assets/move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class move : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{   
+    bool isPressed = false;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed)
+        {
+            player.transform.Translate(0.2f, 0, 0);
+        }
+    }
+    public void onPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+    public void onPoninterOn(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+}
