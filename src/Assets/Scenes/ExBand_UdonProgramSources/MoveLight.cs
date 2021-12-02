4b974cb6e7a1494fe28c85cc30a2e46ebaa02f30
yunminjin2 <56184924+yunminjin2@users.noreply.github.com>
2021-12-02 23:21:34 +0900

    band2

---
+++ src/Assets/Scenes/ExBand_UdonProgramSources/MoveLight.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class MoveLight : UdonSharpBehaviour
+{
+    public GameObject gameObject;
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+
+        gameObject.transform.Translate(0.2f, 0, 0);
+    }
+}
